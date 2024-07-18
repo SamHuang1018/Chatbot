3fd285533b2de88bb5fcc42b224b7ffd9cb06ffc
Bo-Han Huang <120561501+SamHuang1018@users.noreply.github.com>
2024-07-18 14:40:29 +0800

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,17 @@
+#####API_KEY#####
+openai_api_key = ''
+ragic_api_key = ''
+wheel_size_api_key = ''
+channel_access_token = ''
+channel_secret = ''
+
+
+#####Config#####
+model_name = "gpt-4o-2024-05-13"
+temperature = 0
+top_p = 0
+wheel_size_base_url = ''
+tire_product_ragic_url = ''
+bolt_pattern_product_ragic_url = ''
+user_data_ragic_url = ''
+firebase_url = ''
